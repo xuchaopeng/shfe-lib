c8a17ced4606a8420a4d48783802791e09a7b8b9
lizhigaomac <lizhigao@021.com>
2018-09-25 19:26:32 +0800

    feat(更新): 资源文件配置处理，如css、image等

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: path.resolve(__dirname, '../src/index.js')
   },
@@ -8,5 +9,42 @@ module.exports = {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].bundle.js'
   },
-  mode: 'development'
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
+import './index.css';
+import dz from './dz.gif';
 
 function comp() {
   let ele = document.createElement('h2');
   ele.innerHTML = _.join(['Hello', 'webpack!'], ' ');
+  // 新增样式
+  ele.classList.add('hello');
+
+  // 添加图片文件
+  let dzImg = new Image();
+  dzImg.src = dz;
+  ele.appendChild(dzImg);
   return ele;
 }
 
