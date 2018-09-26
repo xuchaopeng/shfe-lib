8ba4f3e0ae6f317709977ffcdb1c9882b8d0ae72
lizhigaomac <lizhigao@021.com>
2018-09-26 16:46:00 +0800

    feat(更新): 实现HMR 热替换
    实现HMR 热替换

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 const myresolve = p => {
   return path.resolve(__dirname, p);
@@ -8,28 +9,35 @@ const myresolve = p => {
 const BUNDLE_PATH = myresolve('../dist');
 
 module.exports = {
-  entry: {
-    print: myresolve('../src/print.js'),
-    index: myresolve('../src/index.js')
-  },
+  entry: ['webpack-hot-middleware/client', myresolve('../src/index.js')],
   output: {
     path: BUNDLE_PATH,
-    filename: '[name].bundle.js',
+    filename: 'index.bundle.js',
     publicPath: myresolve('/')
   },
   mode: 'development',
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: BUNDLE_PATH,
-    // useLocalIp: true,
-    watchContentBase: true,
-    port: 8888
+  // devServer: {
+  //   contentBase: BUNDLE_PATH,
+  //   // useLocalIp: true,
+  //   // watchContentBase: true,
+  //   hot: true,
+  //   port: 8888
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin([BUNDLE_PATH]),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/index.html'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import _ from 'lodash';
 import printMe from './print';
+import './index.css';
 
 function comp() {
   let ele = document.createElement('h2');
   let btn = document.createElement('button');
   let br = document.createElement('br');
 
-  ele.innerHTML = _.join(['Hello', 'webpack-dev-middleware!'], ' ');
+  ele.innerHTML = _.join(['Hello', 'webpack-dev-server!!!'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!';
+  btn.innerHTML = 'Click me and check the console!!!';
   btn.onclick = printMe;
 
   ele.appendChild(br);
@@ -17,5 +18,15 @@ function comp() {
 
   return ele;
 }
+let element = comp();
+document.getElementById('root').appendChild(element);
 
-document.getElementById('root').appendChild(comp());
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    // printMe();
+    document.getElementById('root').removeChild(element);
+    element = comp();
+    document.getElementById('root').appendChild(element);
+  });
+}
