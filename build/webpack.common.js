7f650bf6c36194f85afae5b006e27199eb75bebc
lizhigaomac <lizhigao@021.com>
2018-10-15 14:57:12 +0800

    update

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const myresolve = p => {
@@ -12,10 +11,10 @@ module.exports = {
     app: myresolve('../src/index.js')
   },
   output: {
-    filename: '[name].[contenthash:6].js',
-    // chunkFilename: '[name].[contenthash].js',
-    path: myresolve('../dist/static/js'),
-    publicPath: './static/js/'
+    filename: 'static/js/[name].[contenthash:6].js',
+    chunkFilename: 'static/js/[name].[contenthash].js',
+    path: myresolve('../dist'),
+    publicPath: './'
   },
   optimization: {
     runtimeChunk: {
@@ -40,11 +39,8 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin([myresolve('../dist')], {
-      root: path.join(__dirname, '..')
-    }),
     new HtmlWebpackPlugin({
-      filename: '../index.html',
+      filename: 'index.html',
       template: 'src/index.html'
     }),
     new webpack.ProvidePlugin({
